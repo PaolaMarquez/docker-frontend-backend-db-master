91524af7c040d69bb05573bd6c32262044249f1c
PaolaMarquez <paola.marquez@correo.unimet.edu.ve>
2024-05-03 23:53:02 -0400

    Update home.jsx

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -59,7 +59,7 @@ const Home = () => {
     <>
       <Card>
         <CardBody>
-          <CardTitle tag="h1">Cambie algo</CardTitle>
+          <CardTitle tag="h1">Epale</CardTitle>
           <ListGroup>
             {todos.map((todo) => {
               return (
